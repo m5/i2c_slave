3f8599975a8ed4f2cd3dc9e1bd0e87280de45cc0
m5 <mfivecoa@purdue.edu>
2010-02-26 21:35:29 -0500

    created new controller and timer

--- source/i2c_scl_cntr.vhd
+++ source/i2c_scl_cntr.vhd
@@ -9,7 +9,7 @@ entity i2c_scl_cntr is
     scl         : in  std_logic;
     rst_n       : in  std_logic;
     cnt_en      : in  std_logic;
-    ack_cnt_en  : in std_logic;
+    ack_cnt_en  : in  std_logic;
     stop_rcving : out std_logic
     );
 end i2c_scl_cntr;
@@ -25,20 +25,49 @@ architecture structural of i2c_scl_cntr is
       data_out : out std_logic_vector(7 downto 0));
   end component;
 
+  component edge_detector
+    port (
+      clk     : in  std_logic;
+      rst     : in  std_logic;
+      sig     : in  std_logic;
+      enable  : in  std_logic;
+      rising  : out std_logic;
+      falling : out std_logic);
+  end component;
+
   signal count     : std_logic_vector(7 downto 0);
-  signal count_clk : std_logic;
   signal stop_flag : std_logic;
   signal cnt_reset : std_logic;
+  signal rising    : std_logic;
+  signal falling   : std_logic;
 
 begin
-  stop_flag   <= '1' when count = b"00001000" else '0';
-  count_clk   <= scl when stop_flag = '0'     else '0';
+  -----------------------------------------------------------------------------
+  -- i2c_scl_cntr:  This counter counts up on the falling edge of SCL.
+  --
+  -- The controller expects STOP_RCVING to be set on the falling edge of SCL
+  -- between the 8th bit, and the ACK cycle.
+  --
+  -- As soon as the ACK cycle is over, STOP_RCVING should be deasserted, to let
+  -- the controller know that it's time for a new tx cycle
+  -----------------------------------------------------------------------------  
+  stop_flag   <= '1'     when count = b"00001001" else '0';
   stop_rcving <= stop_flag;
-  cnt_reset <= not cnt_en;
-  
+  cnt_reset   <= not cnt_en
+                 or '1' when count = b"00001010" else '0';
+
+  edge_detector_1 : edge_detector
+    port map (
+      clk     => clk,
+      rst     => rst_n,
+      sig     => scl,
+      enable  => cnt_en,
+      rising  => rising,
+      falling => falling);
+
   op_register_8b_1 : op_register_8b
     port map (
-      clk      => count_clk,
+      clk      => falling,
       rst      => cnt_reset,
       op       => "001",                -- op reg in increment mode
       in_en    => b"00000000",
--- source/i2c_slave_ctrl.vhd
+++ source/i2c_slave_ctrl.vhd
@@ -24,16 +24,15 @@ end i2c_slave_ctrl;
 
 architecture behavioral of i2c_slave_ctrl is
   type state_type is (
-    bored,
-    dazed,
-    curious,
-    ack_will_speak,
-    nack_listen,
-    speech_prep1,
-    speech_prep2,
-    speaking,
-    check_heard,
-    yawning
+    idle,
+    start_on,
+    address_loading,
+    address_full,
+    send_ack,
+    transmit_data,
+    done_transmitting,
+    receive_ack,
+    ack_received
     );
 
   signal state      : state_type;
@@ -58,93 +57,65 @@ begin
            rw_flag,
            addr_match)
   begin
-    case state is
-      when bored => if start_flag = '1' then
-                      next_state <= dazed;
-                    else
-                      next_state <= bored;
-                    end if;
-      when dazed => if start_flag = '1' then
-                      next_state <= dazed;
-                    else
-                      next_state <= curious;
-                    end if;
-
-      when curious => if start_flag = '1' or stop_flag = '1' then
-                        next_state <= yawning;
-                      else
-                        if stop_rcving = '1' then
-                          if addr_match = '1' then
-                            if rw_flag = '1' then
-                              next_state <= ack_will_speak;
-                            else
-                              next_state <= nack_listen;
-                            end if;
-                          else
-                            next_state <= bored;
-                          end if;
-                        else
-                          next_state <= curious;
-                        end if;
-                      end if;
-
-      when nack_listen => if start_flag = '1' or stop_flag = '1' then
-                          next_state <= yawning;
-                        else
-                          if mack_flag <= '1' then
-                            next_state <= bored;
-                          else
-                            next_state <= nack_listen;
-                          end if;
-                        end if;
-                        
-
-      when ack_will_speak => if start_flag = '1' or stop_flag = '1' then
-                               next_state <= yawning;
+    if (not (state = idle or state = start_on) and start_flag = '1')
+      or stop_flag = '1' then
+      next_state <= idle;
+    else
+      case state is
+        when idle => if start_flag = '1' then
+                       next_state <= start_on;
+                     else
+                       next_state <= idle;
+                     end if;
+                     
+        when start_on => if start_flag = '1' then
+                           next_state <= start_on;
+                         else
+                           next_state <= address_loading;
+                         end if;
+                         
+        when address_loading => if stop_rcving = '1' then
+                                  next_state <= address_full;
+                                else
+                                  next_state <= address_loading;
+                                end if;
+                                
+        when address_full => if addr_match = '1' and rw_flag = '1' then
+                               next_state <= send_ack;
                              else
-                               if mack_flag <= '0' then
-                                 next_state <= speech_prep1;
-                               else
-                                 next_state <= ack_will_speak;
-                               end if;
+                               next_state <= idle;
                              end if;
 
-      when check_heard => if start_flag = '1' or stop_flag = '1' then
-                            next_state <= yawning;
-                          else
-                            if mack_flag <= '1' then
-                              next_state <= speech_prep1;
-                            else
-                              next_state <= check_heard;
-                            end if;
-                          end if;
-                          
-      when speech_prep1 => if start_flag = '1' or stop_flag = '1' then
-                             next_state <= yawning;
-                           else
-                             next_state <= speech_prep2;
-                           end if;
-
-      when speech_prep2 => if start_flag = '1' or stop_flag = '1' then
-                             next_state <= yawning;
-                           else
-                             next_state <= speaking;
-                           end if;
-                           
-      when speaking => if start_flag = '1' or stop_flag = '1' then
-                         next_state <= yawning;
-                       else
-                         if stop_rcving = '1' then
-                           next_state <= check_heard;
+        when send_ack => if stop_rcving = '0' then
+                           next_state <= transmit_data;
                          else
-                           next_state <= speaking;
+                           next_state <= send_ack;
                          end if;
-                       end if;
 
-      when yawning => next_state <= bored;
+        when transmit_data => if stop_rcving = '1' then
+                                next_state <= receive_ack;
+                              else
+                                next_state <= transmit_data;
+                              end if;
 
-      when others => next_state <= yawning;
-    end case;
+        when receive_ack => if stop_rcving = '0' then
+                              next_state <= idle;
+                            elsif mack_flag = '0' then
+                              next_state <= ack_received;
+                            else
+                              next_state <= receive_ack;
+                            end if;
+
+        when ack_received => if stop_rcving = '0' then
+                               next_state <= transmit_data;
+                             else
+                               next_state <= ack_received;
+                             end if;
+
+        when others => next_state <= idle;
+
+      end case;
+    end if;
   end process;
 
   process (state,
@@ -156,8 +127,15 @@ begin
            addr_match)
   begin
     case state is
-      when bored =>
-        sda_ack    <= '1';
+      -------------------------------------------------------------------------
+      -- Moore outputs
+      -------------------------------------------------------------------------
+      
+      -------------------------------------------------------------------------
+      -- idle: Don't do anything. Disable all outputs.
+      -------------------------------------------------------------------------
+      when idle =>
+        sda_ack    <= 'Z';
         sda_out_en <= '0';
         rxsr_ctrl  <= '0';
         txsr_ctrl  <= b"00";
@@ -165,97 +143,147 @@ begin
         ack_cnt_en <= '0';
         tx_renable <= '0';
         
-      when dazed =>
-        sda_ack    <= '1';
+      -------------------------------------------------------------------------
+      -- start_on: another idle state. We're just waiting for the start
+      --           flag to clear.
+      -------------------------------------------------------------------------
+      when start_on =>
+        sda_ack    <= 'Z';
         sda_out_en <= '0';
         rxsr_ctrl  <= '0';
         txsr_ctrl  <= b"00";
         cnt_en     <= '0';
         ack_cnt_en <= '0';
         tx_renable <= '0';
-        
-      when curious =>
-        sda_ack    <= '1';
+
+      -------------------------------------------------------------------------
+      -- address_loading: This begins when SCL goes LOW for the first time,
+      --                    before the first address bit is sent.
+      --
+      --            1) Load the address and rw bit into rxsr
+      --            2) Start the scl counter, so we'll know when it's full
+      -------------------------------------------------------------------------
+      when address_loading =>
+        sda_ack    <= 'Z';
         sda_out_en <= '0';
         rxsr_ctrl  <= '1';
         txsr_ctrl  <= b"00";
         cnt_en     <= '1';
         ack_cnt_en <= '0';
         tx_renable <= '0';
-        
-      when nack_listen =>
+
+      -------------------------------------------------------------------------
+      -- address_full: This begins as soon as SCL goes LOW after the R/W bit is
+      --                 stored in rxsr. Nothing new needs to be done here.
+      -------------------------------------------------------------------------
+      when address_full =>
+        sda_ack    <= 'Z';
+        sda_out_en <= '0';
+        rxsr_ctrl  <= '1';
+        txsr_ctrl  <= b"00";
+        cnt_en     <= '1';
+        ack_cnt_en <= '0';
+        tx_renable <= '0';
+
+      -------------------------------------------------------------------------
+      -- send_ack: This begins immediately after we have confirmed that the
+      --           master is talking to us, and wants us to transmit.
+      --           SCL is still low between 8th and 9th bits.
+      --
+      --      1) Pull sda_ack to 0
+      --      2) Keep sda_out_en at 0
+      --
+      --   Also, we need to load the txsr with the next byte
+      --      3) Set txsr_ctrl to "11", load. Data will be loaded when SCL goes
+      --         LOW after ACK is sent.
+      -------------------------------------------------------------------------
+      when send_ack =>
         sda_ack    <= '0';
         sda_out_en <= '0';
         rxsr_ctrl  <= '0';
-        txsr_ctrl  <= b"00";
-        cnt_en     <= '0';
+        txsr_ctrl  <= b"11";
+        cnt_en     <= '1';
         ack_cnt_en <= '1';
         tx_renable <= '0';
-        
-      when ack_will_speak =>
-        sda_ack    <= '0';
-        sda_out_en <= '0';
-        rxsr_ctrl  <= '0';
-        txsr_ctrl  <= b"00";
-        cnt_en     <= '0';
-        ack_cnt_en <= '1';
-        tx_renable <= '0';
-        
-      when check_heard =>
-        sda_ack    <= '1';
-        sda_out_en <= '0';
-        rxsr_ctrl  <= '0';
-        txsr_ctrl  <= b"00";
-        cnt_en     <= '0';
-        ack_cnt_en <= '1';
-        tx_renable <= '0';
-        
-      when speech_prep1 =>
-        sda_ack    <= '1';
+
+      -------------------------------------------------------------------------
+      -- transmit_data: This begins as SCL goes LOW between the 9th bit of the
+      --                previous cycle, and the first bit of the new one.
+      --
+      --      1) Set txsr_ctrl to "10". This will shift the next bit onto
+      --         SDA_OUT on the falling edge of each SCL
+      --      2) Set sda_out_en to 1. This switches SDA_OUT from our ACK
+      --         flag to the txsr's output.
+      --      3) Set ack_cnt_en to 1, so we'll know when to quit.
+      -------------------------------------------------------------------------
+      when transmit_data =>
+        sda_ack    <= 'Z';
         sda_out_en <= '1';
+        rxsr_ctrl  <= '0';
+        txsr_ctrl  <= b"10";
+        cnt_en     <= '1';
+        ack_cnt_en <= '0';
+        tx_renable <= '0';
+
+      -------------------------------------------------------------------------
+      -- receive_ack: This starts as SCL goes LOW between the 8th bit, and the
+      --              ACK cycle.
+      --
+      --      1) Set ack_cnt_enable, so the counter will know we're waiting on
+      --         an ACK
+      --      2) Turn off sda_out_en, in case it would interfere with the master
+      --         ACK transmission.
+      --
+      --   Since the tx cycle completed, we need to tell the txfifo that we
+      --   sent the data, so:
+      --
+      --      3) Set tx_renable. This will decrement the read pointer when SCL
+      --         goes to HIGH at the beginning of the ACK cycle
+      -------------------------------------------------------------------------
+      when recieve_ack =>
+        sda_ack    <= 'Z';
+        sda_out_en <= '0';
+        rxsr_ctrl  <= '0';
+        txsr_ctrl  <= b"00";
+        cnt_en     <= '1';
+        ack_cnt_en <= '1';
+        tx_renable <= '1';
+        
+      -------------------------------------------------------------------------
+      -- ack_received: this starts as soon as an ACK is detected from the
+      --               master. This should happen near the beginning of the ACK
+      --               cycle, while SCL is still high.
+      --
+      --      Since we received an ACK, we can load tx data for a new cycle. If
+      --      we end up receiving a STOP, it doesn't matter, because we don't
+      --      tell the txfifo that we read the data until after we've sent it.
+      --
+      --      1) Set txsr_ctrl to b"11", to load the new data from the txfifo,
+      --         at the end of the ACK cycle.
+      -------------------------------------------------------------------------
+      when ack_received =>
+        sda_ack    <= 'Z';
+        sda_out_en <= '0';
         rxsr_ctrl  <= '0';
         txsr_ctrl  <= b"11";
         cnt_en     <= '0';
         ack_cnt_en <= '0';
-        tx_renable <= '0';
-        
-      when speech_prep2 =>
-        sda_ack    <= '1';
-        sda_out_en <= '1';
-        rxsr_ctrl  <= '0';
-        txsr_ctrl  <= b"00";
-        cnt_en     <= '0';
-        ack_cnt_en <= '0';
         tx_renable <= '1';
-        
-      when speaking =>
-        sda_ack    <= '1';
-        sda_out_en <= '1';
-        rxsr_ctrl  <= '0';
-        txsr_ctrl  <= "01";
-        cnt_en     <= '0';
-        ack_cnt_en <= '0';
-        tx_renable <= '0';
-        
-      when yawning =>
-        sda_ack    <= '1';
-        sda_out_en <= '0';
-        rxsr_ctrl  <= '0';
-        txsr_ctrl  <= b"00";
-        cnt_en     <= '0';
-        ack_cnt_en <= '0';
-        tx_renable <= '0';
-        
+
+      -------------------------------------------------------------------------
+      -- others: This should never happen. If it does, treat it as an idle. It
+      --         will be sent to idle on the next clock cycle.
+      -------------------------------------------------------------------------
       when others =>
-        sda_ack    <= '1';
+        sda_ack    <= 'Z';
         sda_out_en <= '0';
         rxsr_ctrl  <= '0';
         txsr_ctrl  <= b"00";
         cnt_en     <= '0';
         ack_cnt_en <= '0';
         tx_renable <= '0';
-      end case;
-    end process;
+    end case;
+  end process;
 
 end behavioral;
 
